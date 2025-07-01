acfa068897e08ae709a7d0ed45576a58abd12875
Networksentinel <network.sentinel@protonmail.com>
2025-07-01 14:21:15 +0200

    chore: created tailwind.config and extend the font-family to include robot bold and regular which I imported to style.css

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  theme: {
+    extend: {
+      fontFamily: {
+        RobotoBold: ['RobotoBold', 'sans-serif'],
+        RobotoRegular: ['RobotoRegular', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
